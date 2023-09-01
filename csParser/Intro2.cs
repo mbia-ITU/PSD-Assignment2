b1ba0990c971b40b6f5e1348a7efc8bbd7ad5dd2
emno <emno@itu.dk>
2023-09-01 10:29:12 +0200

    solve eval and simplify

--- csParser/Intro2.cs
+++ csParser/Intro2.cs
@@ -1,5 +1,9 @@
 namespace Intro;
-class Expr { }
+using System.Linq;
+abstract class Expr { 
+    public abstract int Eval(List<(string, int)> env);
+    public abstract Expr Simplify();
+}
 
 class CstI : Expr
 {
@@ -8,7 +12,12 @@ class CstI : Expr
     {
         value = val;
     }
+
+    public override int Eval(List<(string, int)> env) => value;
+
     public override string ToString() => value.ToString();
+
+    public override Expr Simplify() => new CstI(value);
 
 }
 
@@ -19,8 +28,12 @@ class Var : Expr
     {
         name = s;
     }
+    public override int Eval(List<(string, int)> env) => env.Where(c => c.Item1 == name).FirstOrDefault().Item2;
+    
 
     public override string ToString() => name;
+
+    public override Expr Simplify() => new Var(name);
 }
 
 abstract class Binop : Expr
@@ -45,16 +58,63 @@ class Add : Binop
     public Add(Expr exp1, Expr exp2) : base(exp1, exp2) { }
 
     public override string ToString() => Print("+");
+
+    public override int Eval(List<(string, int)> env) => exp1.Eval(env) + exp2.Eval(env);
+
+    public override Expr Simplify(){
+        var simple1 = exp1.Simplify();
+        var simple2 = exp2.Simplify();
+        if (simple1 == new CstI(0)){
+            return simple2;
+        }
+        if (simple2 == new CstI(0)){
+            return simple1;
+        }
+        return new Add(simple1, simple2);
+    }
 }
 class Sub : Binop
 {
     public Sub(Expr exp1, Expr exp2) : base(exp1, exp2) { }
     public override string ToString() => Print("-");
+
+    public override int Eval(List<(string, int)> env) => exp1.Eval(env) - exp2.Eval(env);
+    public override Expr Simplify(){
+        var simple1 = exp1.Simplify();
+        var simple2 = exp2.Simplify();
+        if (simple2 == new CstI(0)){
+            return simple1;
+        }
+        if (simple1 == simple2){
+            return new CstI(0);
+        }
+        return new Sub(simple1, simple2);
+    }
 }
 class Mul : Binop
 {
     public Mul(Expr exp1, Expr exp2) : base(exp1, exp2) { }
     public override string ToString() => Print("*");
+
+    public override int Eval(List<(string, int)> env) => exp1.Eval(env) * exp2.Eval(env);
+
+    public override Expr Simplify(){
+        var simple1 = exp1.Simplify();
+        var simple2 = exp2.Simplify();
+        if (simple1 == new CstI(0)){
+            return new CstI(0);
+        }
+        if (simple2 == new CstI(0)){
+            return new CstI(0);
+        }
+        if (simple1 == new CstI(1)){
+            return simple2;
+        }
+        if (simple2 == new CstI(1)){
+            return simple1;
+        }
+        return new Mul(simple1, simple2);
+    }
 }
 
 class Program
@@ -67,6 +127,7 @@ class Program
         Console.WriteLine(e1);
         Console.WriteLine(e2);
         Console.WriteLine(e3);
+        
     }
 }
 
